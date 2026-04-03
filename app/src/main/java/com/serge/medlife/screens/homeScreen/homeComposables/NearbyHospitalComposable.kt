6d2de3751292896534813640435bf1e3a74e4c2d
serge-x404 <kpdev1124@gmail.com>
2026-04-03 12:43:45 +0530

    NearbyHospitalComposable

---
+++ app/src/main/java/com/serge/medlife/screens/homeScreen/homeComposables/NearbyHospitalComposable.kt
@@ -0,0 +1,57 @@
+package com.serge.medlife.screens.homeScreen.homeComposables
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun NearbyHospitalComposable(navigateToHospital: () -> Unit ) {
+    Button(
+        onClick = { navigateToHospital() },
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.surfaceContainerHighest),
+        border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.outline),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .padding(horizontal = 30.dp),
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.weight(0.1f)
+        ) {
+            Text(
+                text = "Nearby Hospital",
+                color = MaterialTheme.colorScheme.onBackground,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = "Look for hospitals nearby",
+                color = MaterialTheme.colorScheme.onBackground,
+                style = MaterialTheme.typography.labelSmall
+            )
+        }
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+            contentDescription = "Arrow Forward",
+            tint = MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
--- app/src/main/java/com/serge/medlife/screens/homeScreen/homeScreen.kt
+++ app/src/main/java/com/serge/medlife/screens/homeScreen/homeScreen.kt
@@ -2,13 +2,9 @@ package com.serge.medlife.screens.homeScreen
 
 import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -21,10 +17,7 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -38,7 +31,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -50,11 +42,11 @@ import com.serge.medlife.rtdb.DoctorDetailsDTO
 import com.serge.medlife.rtdb.RTDB
 import com.serge.medlife.screens.class_objects.BestSellingProducts
 import com.serge.medlife.screens.class_objects.Hot
-import com.serge.medlife.screens.class_objects.hospitals
 import com.serge.medlife.screens.homeScreen.homeComposables.CardServicesHomeScreen
 import com.serge.medlife.screens.homeScreen.homeComposables.ConsultDocComposable
 import com.serge.medlife.screens.homeScreen.homeComposables.DoctorData
 import com.serge.medlife.screens.homeScreen.homeComposables.HeaderBox
+import com.serge.medlife.screens.homeScreen.homeComposables.NearbyHospitalComposable
 import com.serge.medlife.screens.homeScreen.homeComposables.SearchBox
 import com.serge.medlife.screens.homeScreen.homeComposables.Title
 import com.serge.medlife.screens.homeScreen.homeComposables.categoriesHomeScreen
@@ -221,64 +213,7 @@ fun HomeScreen(
                         }
                     }
                     Spacer(Modifier.height(30.dp))
-
-
-                    Title("Nearby Hospitals")
-
-
-                    Spacer(Modifier.height(5.dp))
-                    LazyRow(
-                        modifier = Modifier.padding(start = 26.dp),
-                        horizontalArrangement = Arrangement.spacedBy(12.dp)
-                    ) {
-                        items(hospitals.images) {
-                            Box(
-                                modifier = Modifier
-                                    .size(180.dp)
-                                    .clickable(
-                                        enabled = true,
-                                        onClick = {
-                                            navigateToHospital()
-                                        })
-                                    .border(
-                                        1.dp, color = MaterialTheme.colorScheme.outline,
-                                        shape = RoundedCornerShape(4.dp)
-                                    )
-                                    .background(MaterialTheme.colorScheme.surfaceContainer)
-                            ) {
-//                    Surface(shape = CircleShape,
-//                        modifier = Modifier
-//                            .offset(x = 20.dp, (-10).dp)
-//                            .align(Alignment.TopEnd)
-//                            .size(60.dp)) {}
-                                Column(modifier = Modifier.padding(12.dp)) {
-                                    Image(
-                                        painter = painterResource(it.img),
-                                        contentDescription = null,
-                                        modifier = Modifier.size(60.dp)
-                                    )
-                                    Text(
-                                        it.name,
-                                        color = MaterialTheme.colorScheme.onBackground,
-                                        style = MaterialTheme.typography.labelLarge
-                                    )
-                                    Spacer(Modifier.height(8.dp))
-                                    Row(verticalAlignment = Alignment.CenterVertically) {
-                                        Text(
-                                            "See maps",
-                                            style = MaterialTheme.typography.bodyLarge,
-                                            color = MaterialTheme.colorScheme.onBackground
-                                        )
-                                        Icon(
-                                            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
-                                            contentDescription = null,
-                                            tint = MaterialTheme.colorScheme.onBackground
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    NearbyHospitalComposable(navigateToHospital)
                     Spacer(Modifier.height(30.dp))
 
 
