ab107acde1e4582d0be7fdca72ec58598780c5b3
serge-x404 <kpdev1124@gmail.com>
2026-04-07 16:12:50 +0530

    Change: homeScreen composable changes

--- app/src/main/java/com/serge/medlife/screens/homeScreen/homeComposables/NearbyHospitalComposable.kt
+++ app/src/main/java/com/serge/medlife/screens/homeScreen/homeComposables/NearbyHospitalComposable.kt
@@ -1,57 +1,80 @@
 package com.serge.medlife.screens.homeScreen.homeComposables
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.serge.medlife.R
 
 
 @Composable
 fun NearbyHospitalComposable(navigateToHospital: () -> Unit ) {
-    Button(
+    Card(
         onClick = { navigateToHospital() },
-        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.surfaceContainerHighest),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainerHighest),
         border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.outline),
         modifier = Modifier
-            .fillMaxWidth()
-            .height(80.dp)
             .padding(horizontal = 30.dp),
         shape = RoundedCornerShape(10.dp)
     ) {
         Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.Start,
-            modifier = Modifier.weight(0.1f)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 30.dp)
+                .padding(bottom = 12.dp)
         ) {
-            Text(
-                text = "Nearby Hospital",
-                color = MaterialTheme.colorScheme.onBackground,
-                style = MaterialTheme.typography.titleMedium
+            Image(
+                painter = painterResource(R.drawable.pharmacy),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .align(Alignment.CenterHorizontally)
             )
-            Text(
-                text = "Look for hospitals nearby",
-                color = MaterialTheme.colorScheme.onBackground,
-                style = MaterialTheme.typography.labelSmall
-            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(
+                        text = "Nearby Hospital",
+                        color = MaterialTheme.colorScheme.onBackground,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Text(
+                        text = "Look for hospitals nearby",
+                        color = MaterialTheme.colorScheme.onBackground,
+                        style = MaterialTheme.typography.labelSmall
+                    )
+                }
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                    contentDescription = "Arrow Forward",
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+            }
         }
-        Icon(
-            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
-            contentDescription = "Arrow Forward",
-            tint = MaterialTheme.colorScheme.onBackground
-        )
     }
 }
